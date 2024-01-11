38461069c80e1ece8327cd50d43b8a28f7fcd702
Irene <Irene@PC-CLASE>
2024-01-11 12:28:10 +0100

    Temario Cadenas

---
+++ actividadesClase/src/cadenas/Ejemplo7Cadenas.java
@@ -0,0 +1,34 @@
+package cadenas;
+
+import java.util.Scanner;
+
+public class Ejemplo7Cadenas {
+	
+	public static void main(String[] args) {
+		final String prefijo = "Javalín, javalón";
+		final String sufijo = "javalén, len, len";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escriba una frase: ");
+		String entrada = sc.nextLine();
+		
+		boolean idiomaJavalandia = false;
+		
+		if (entrada.startsWith(prefijo)) {
+			idiomaJavalandia = true;
+			entrada = entrada.substring(prefijo.length());
+		} else if (entrada.endsWith(sufijo)) {
+			idiomaJavalandia = true;
+			entrada = entrada.substring(0, entrada.length() - sufijo.length());
+		}
+		
+		if (idiomaJavalandia) {
+			entrada = entrada.strip();
+			System.out.println(entrada);
+		} else {
+			System.out.println("No está escrito en el idioma Javalandia");
+		}
+		
+	}
+
+}
